6937494ded58a5de84049b77afbb3ab8b5d17afd
Delvira <hexeluna@hotmail.com>
2015-05-12 14:33:42 +0200

    ...

--- src/dotandboxes/Gameboard.java
+++ src/dotandboxes/Gameboard.java
@@ -6,10 +6,9 @@
 package dotandboxes;
 
 import java.awt.Color;
-import java.awt.Component;
-import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.util.List;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -22,8 +21,7 @@ import javax.swing.JPanel;
 public class Gameboard extends JPanel{
 
     
-    private int rows;
-    private int columns;
+
     private GridBagLayout grid;
     GridBagConstraints gridBag;
     
@@ -33,15 +31,13 @@ public class Gameboard extends JPanel{
      * @param rows
      * @param columns 
      */
-    public Gameboard(int rows, int columns) {
+    public Gameboard(List<List> list) {
         super();
-        this.rows = rows;
-        this.columns = columns;
         this.setBackground(Color.WHITE);
         grid = new GridBagLayout();
         this.setLayout(grid);
         gridBag = new GridBagConstraints();
-        createGrid();
+        createGrid(list);
         this.setVisible(true);
     }
     
@@ -51,29 +47,46 @@ public class Gameboard extends JPanel{
      * @param rows
      * @param columns 
      */
-    private void createGrid() {
-        for (int y = 0; y <= (2*columns); y++) {
-            for (int x = 0; x <= (2*rows); x++) {
-                gridBag.gridy = y;
-                gridBag.gridx = x;
-                if ((y%2 == 0) && (x%2 == 0)) {
-                    gridBag.fill = GridBagConstraints.CENTER;
-                    this.add(new DotPicture(), gridBag);
-                }
-                else if ((y%2 == 0) && (x%2 != 0)) {
-                    gridBag.fill = GridBagConstraints.HORIZONTAL;
-                    this.add(new LinePicture(LinePicture.HORIZONTAL), gridBag);
-                }
-                else if ((y%2 != 0) && (x%2 == 0)) {
-                    gridBag.fill = GridBagConstraints.VERTICAL;
-                    this.add(new LinePicture(LinePicture.VERTICAL), gridBag);
-                }
-                else {
-                    gridBag.fill = GridBagConstraints.BOTH;
-                    this.add(new BoxPicture(), gridBag);
-                }
+    private void createGrid(List list) {
+        
+        // Setze ersten Dot
+        gridBag.gridx = 0;
+        gridBag.gridy = 0;
+        this.add(new DotPicture(), gridBag);
+        
+        // Setze erste Spalte mit Lines
+        for (int y = 0; y <= list.get(0).size; y++) {
+            gridBag.gridy = 2*y+1;
+            this.add(new LinePicture(list.get(0).get(y).getLeftLine(), LinePicture.VERTICAL), gridBag);
+            gridBag.gridy += 1;
+            this.add(new DotPicture(), gridBag);
+        }
+        
+        // Setze erste Zeile
+        gridBag.gridy = 0;
+        for (int x = 0; x <= list.size(); x++) {
+            gridBag.gridx = 2*x+1;
+            this.add(new LinePicture(list.get(x).get(0).getUpperLine(), LinePicture.HORIZONTAL), gridBag);
+            gridBag.gridx += 1;
+            this.add(new DotPicture(), gridBag);
+        }
+        
+        // Setze Rest
+        for (int x = 1; x <= list.size(); x++) {
+            gridBag.gridx = 2*x+1;
+            for (int y = 1; y <= list.get(x).size(); y++) {
+                Box box = list.get(x).get(y);
+                gridBag.gridy = 2*y+1;
+                this.add(new BoxPicture(box), gridBag);
+                gridBag.gridy += 1;     // Untere Linie
+                this.add(new LinePicture(box.getBottomLine(), LinePicture.HORIZONTAL),gridBag);
+                gridBag.gridx += 1;     // Punkt unten rechts
+                this.add(new DotPicture(), gridBag);
+                gridBag.gridy -= 1;     // Rechte Linie
+                this.add(new LinePicture(box.getRightLine(), LinePicture.VERTICAL), gridBag);
             }
         }
+        
     }
     
     
@@ -84,7 +97,7 @@ public class Gameboard extends JPanel{
     public static void main(String[] args) {
         JFrame frame = new JFrame("Test");
         Gameboard game = new Gameboard(10,10);
-        frame.setContentPane(game);
+        frame.add(game);
         frame.setSize(600, 400);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
